ac15cbb0b9e8d6a62c5688c271c10a565cb7e7d7
liyi <Liyiaiai230@gmail.com>
2021-01-19 19:21:52 +0800

    up

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -11,6 +11,48 @@ module.exports = {
             title: "狸衣",
             template: "src/assets/index.html"
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ["file-loader"]
+            },
+            {
+                test: /\.styl$/,
+                use: [{
+                    loader: "style-loader"
+                }, {
+                    loader: "css-loader"
+                }, {
+                    loader: "stylus-loader"
+                }]
+            },
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: "style-loader"
+                }, {
+                    loader: "css-loader"
+                }, {
+                    loader: "less-loader"
+                }]
+            },
+
+            {
+                test: /\.scss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    {
+                        loader: "sass-loader",
+                        options:{
+                            implementation: require('dart-sass'),
+                        },
+                    },
+                ],
+            },
+        ],
+    },
 };
 
